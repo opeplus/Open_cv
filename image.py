df8fbb423dedcf9592502e4d4071a8ece16c9f69
opeplus <opeplus4@gmail.com>
2024-07-17 14:11:58 -0700

    A program that the detects image i.e face and frames the image in a green rectangular frame, convert the image to gray and detects  motion pictures i.e video without audio and converts to gray and frames the motion picture in a green rectangular frame

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-image = cv2.imread('picler.jpg')
+image = cv2.imread('picler.jpeg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("Gray", gray)
 # cv2.waitKey()
